9b08b73dfd8c40c9b507f3f47ccd652e4ce9033d
Алексей Шубенков <shubenkov-93@yandex.ru>
2023-10-31 19:56:56 +0300

    add urlpatterns_auth in urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -20,9 +20,12 @@ v1_router.register(
     r'titles/(?P<title_id>[\d]+)/reviews/(?P<review_id>[\w]+)/comments',
     CommentViewSet
 )
+urlpatterns_auth = [
+    path('token/', TokenView.as_view(), name='token'),
+    path('signup/', RegistrationView.as_view(), name='registration'),
+]
 
 urlpatterns = [
     path('v1/', include(v1_router.urls)),
-    path('v1/auth/token/', TokenView.as_view(), name='token'),
-    path('v1/auth/signup/', RegistrationView.as_view(), name='registration'),
+    path('v1/auth/', include(urlpatterns_auth)),
 ]
