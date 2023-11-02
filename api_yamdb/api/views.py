f23df5fb91fa6f7b11b68b2d86ef32938e2ad048
Тимофей Карпов <acuna-think@yandex.ru>
2023-11-02 23:06:54 +0300

    remove unused import of IntegrityError

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.conf import settings
-from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
 
 from django_filters.rest_framework import DjangoFilterBackend
